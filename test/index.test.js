8199c30fb92fb3b7bf868f48b46e32027cccddb7
W. Brian Quinn <w.brian.quinn@gmail.com>
2023-04-28 16:41:15 +0100

    added test

---
+++ test/index.test.js
@@ -0,0 +1,40 @@
+const assert = require('assert');
+const {request} = require('gaxios');
+const {exec} = require('child_process');
+const waitPort = require('wait-port');
+
+const startFF = async (target, signature, port) => {
+  const ff = exec(
+    `npx functions-framework --target=${target} --signature-type=${signature} --port=${port}`
+  );
+  await waitPort({host: 'localhost', port});
+  return ff;
+};
+
+const httpInvocation = (fnUrl, port) => {
+  const baseUrl = `http://localhost:${port}`;
+
+  // GET request
+  return request({
+    url: `${baseUrl}/${fnUrl}`,
+  });
+};
+
+describe('index.test.js', () => {
+  describe('functions_airports_get airportsGET', () => {
+    const PORT = 8081;
+    let ffProc;
+
+    before(async () => {
+      ffProc = await startFF('airportsGET', 'http', PORT);
+    });
+
+    after(() => ffProc.kill());
+
+    it('helloGET: should print hello world', async () => {
+      const response = await httpInvocation('airportsGET', PORT);
+      assert.strictEqual(response.status, 200);
+      assert.strictEqual(response.data, 'Airports of the World!');
+    });
+  });
+});
